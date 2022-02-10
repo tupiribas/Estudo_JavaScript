e8b93679c1b2eaf5482366c8034583bd4293e9cf
Tupi Guedes <tupiguedes@outlook.com>
2022-02-09 23:07:24 -0300

    Rest/Spread

---
+++ esnext/operadorRestSpread.js
@@ -0,0 +1,12 @@
+// operador ... rest(juntar)/spread(espalha)
+// usa rest com parâmetro de função
+
+// Usar spread com objeto 
+const funcionario = { nome: 'Anna', salario: 12333.22 }
+const clone = { ativo: true, ...funcionario }
+console.log(clone);
+
+// Usar spread com array
+const grupoA = [1, 2, 3, 4, 5]
+const grupoFinal = ['outros fulanos anteriores', ...grupoA]
+console.log(grupoFinal);

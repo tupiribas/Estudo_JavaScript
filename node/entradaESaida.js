d7ee7a2f9cc21fa4a4cbc33590b3991deb21bede
Tupi Guedes <tupiguedes@outlook.com>
2022-02-09 22:14:10 -0300

    entrada e saída de dados pelo terminal

---
+++ node/entradaESaida.js
@@ -0,0 +1,22 @@
+// Buscando no terminal o caracter '-a' retornando verdadeiro oou falso
+const anonimo = process.argv.indexOf('-a') !== -1
+console.log(anonimo);
+
+/**
+ * (sendo true) -> irá chamar um procedimento que mostrará na tela do terminal,
+ * caso contrário, ele solicitará pelo terminal o nome do usuário e imprimirá na tela do terminal
+*/
+if (anonimo) {
+    process.stdout.write('Fala anônimo, meu jovem!\n')
+    process.exit()
+} 
+else {
+    process.stdout.write('Informe seu nome: ')
+    process.stdin.on('data', data => {
+        const nome = data.toString()
+        
+        process.stdout.write(`Seja bem-vindo(a) ${nome}!!\n`)
+        process.exit()
+    })
+}
+

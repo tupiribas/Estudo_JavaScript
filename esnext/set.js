809fb901017363b3f6442dadfae96dc882b44678
Tupi Guedes <tupiguedes@outlook.com>
2022-02-12 00:36:37 -0300

    Outros exemplos com Set

--- esnext/set.js
+++ esnext/set.js
@@ -6,3 +6,13 @@ times.add('Vitória')
 times.add('Vitória')
 
 console.log(times);
+console.log(times.size);
+console.log(times.has('vasco')); // false
+console.log(times.has('Vasco')); // true
+
+times.delete('Flamengo')
+console.log(times.has('Flamengo'));
+
+const nomes = ['Raquel', 'Flavia', 'Marcos', 'Flavia']
+const nomesSet = new Set(nomes)
+console.log(nomesSet);

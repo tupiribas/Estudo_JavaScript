f46ce22341f54cea932a97977ff34fe3c8976aed
Tupi Guedes <tupiguedes@outlook.com>
2022-02-13 15:53:11 -0300

    Alterando dados

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -7,11 +7,20 @@ const app = express();
 // Leitura do corpo da requisiçaõ (cria o objeto utilizando o urlcoded())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Fazendo a requisição
+// Inserir dados
 app.post('/usuarios', (req, resp) => {
     console.log("Valor da requisição: \n", req.body);
     // Mandando a resposta renderizado no browser
     resp.send('<h1>Parabens usuário adicionado com sucesso!</h1>')
 })
+
+// Alterar dados
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.params.id);
+    console.log("Valor da requisição: \n", req.body);
+    // Mandando a resposta renderizado no browser
+    resp.send('<h1>Parabens usuário alterado com sucesso!</h1>')
+})
+
 // Inserindo a porta
 app.listen(3003)

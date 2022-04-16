b6cf6760cdc891d7765c37e730b5fff9add36241
Tupi Guedes <tupiguedes@outlook.com>
2022-04-16 11:53:16 -0300

    Requisição POST fazendo upload de arquivos

--- ajax/server.js
+++ ajax/server.js
@@ -7,5 +7,30 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true })) 
 app.use(bodyParser.json())
 
+// Upload de imagens e arquivos
+const multer = require('multer')
+
+// Configurar e personalizar a pasta onde irá salvar o arquivo
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload') // '.' -> pasta de destino 
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo') // single(name="arquivo") (html)
+
+app.post('./upload', (req, resp) => {
+    upload(req, resp, err => {
+        if (err) {
+            return resp.end('ERRO COD.:001 >>> Ocorreu um erro.')
+        }
+
+        resp.end('Concluído com sucesso!')
+    })
+})
+
 app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8000, () => console.log('Servidor inicializado.'))

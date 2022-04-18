3aef1ebc0c3c808db1920e506aacb105e05e9971
Tupi Guedes <tupiguedes@outlook.com>
2022-04-18 20:19:01 -0300

    Fundamentos do Gulp - #2

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -1,11 +1,40 @@
 const gulp = require('gulp');
-// const { series } = require('gulp') Possibilidade
-const series = gulp.series
+const { series, parallel } = require('gulp')
+// const series = gulp.series
 
-// Copiar arquivos ou pastas
-function copiar(callbeck) {
-    console.log('Tarefa de copiar');
+antes1 = callbeck => {
+    console.log('Tarefa Antes 1!');
     return callbeck()
 }
 
-module.exports.default = series(copiar)
+antes2 = callbeck => {
+    console.log('Tarefa Antes 2!');
+    return callbeck()
+}
+
+// Copiar arquivos ou pastas
+function copiar(callbeck) {
+    try {
+        // Arquivos selecionaveis
+        // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+        // Captura todas (**) as pastas e arquivos que contenham .txt
+        gulp.src('pastaA/**/*.txt') 
+            // Aplicar transformações -> cria outra pasta e cola os arquivos nela
+            .pipe(gulp.dest('pastaB')) 
+        return callbeck()
+    }
+    catch(erro) {
+        console.log("ERRO COD.:001 >>> Erro de cópia!", erro);
+    }
+}
+
+fim = callbeck => {
+    console.log('Tarefa Fim.');
+    return callbeck()
+}
+
+module.exports.default = series(
+    parallel(antes1, antes2),
+    copiar, 
+    fim
+)

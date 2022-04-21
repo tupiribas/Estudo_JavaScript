f869e10d29c206e80e3c8b443495c9d8dcc798ef
Tupi Guedes <tupiguedes@outlook.com>
2022-04-20 23:33:49 -0300

    Dependencias task

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -2,14 +2,21 @@
  * Frameworks externos e dentre outros.
  */
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-function depsCSS(callback) {
-    
-    return callback()
+// gulp\spa\node_modules\font-awesome\css\font-awesome.css
+
+function depsCSS() {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({ "uglyComments": false }))
+        .pipe(concat('deps.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-function depsFonts(callback) {
-    return callback()
+function depsFonts() {
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {

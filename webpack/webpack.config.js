57c9c73bea908d8ffc8c2db1232828b370c93cdf
Tupi Guedes <tupiguedes@outlook.com>
2022-04-24 11:13:09 -0300

    Pasta public: Desenvolvimento & produção -> falhas

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production', 
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({

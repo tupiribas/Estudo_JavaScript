1e10c5000980be5798b4ac9dd12b2ec02fff4e1e
Tupi Guedes <tupiguedes@outlook.com>
2022-07-16 20:03:40 -0300

    Multiplos componentes

---
+++ react/exercicios/src/componentes/Multiplos.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Boatarde = props => <h1>Boa tarde, {props.nome}!</h1>
+
+const Boanoite = props => <h1>Boa noite {props.nome}!</h1>
+
+export { Boatarde, Boanoite }
+
+export default {
+    Boatarde,
+    Boanoite
+}
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import Primeiro from "./componentes/Primeiro";
 import Bomdia from "./componentes/Bomdia";
+import Multi, { Boatarde, Boanoite } from "./componentes/Multiplos";
 
-ReactDOM.render(<Bomdia nome="Guilherme" />, document.getElementById('root'))
+// ReactDOM.render(<Bomdia nome="Guilherme" />, document.getElementById('root'))
+ReactDOM.render(
+    <Fragment>
+        <Multi.Boatarde nome="Tupi"/>
+        <Boanoite nome="Mestre"/>
+    </Fragment>    
+, document.getElementById('root'))

5ad15c644c59f29c80ff768c640249217830e882
Tupi Guedes <tupiguedes@outlook.com>
2022-07-17 20:51:12 -0300

    Componente Logo

--- react/crud/frontend/src/components/template/Main.jsx
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -5,9 +5,8 @@ import Header from "./Header";
 
 export default props => 
     <Fragment>
-        <Header>
-            <main className="content">
-                Conteúdo
-            </main>
-        </Header>
+        <Header/>
+        <main className="content">
+            Conteúdo
+        </main>
     </Fragment>

16595ce148a930588efabb192083aa8e32c4efe0
Tupi Guedes <tupiguedes@outlook.com>
2022-07-17 16:24:12 -0300

    Criando e estilizando a display

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+
 import Button from '../components/Button'
+import Display from '../components/Display'
 import './Calculator.css'
 
 export default class Calculator extends Component {
     render() {
         return (
             <div className='calculadora'>
+                <Display value={100}/>
                 <Button label="AC"/>
                 <Button label="/"/>
                 <Button label="7"/>

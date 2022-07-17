93a0140adb437cd291e59db20740346ff2ac6895
Tupi Guedes <tupiguedes@outlook.com>
2022-07-17 17:42:39 -0300

    Implementando a lógica

--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0], // valor ou operação
+    atual: 0 
+}
+
 export default class Calculator extends Component {
+
+    state = { ...initialState }
 
     constructor(props) {
         super(props)
@@ -15,7 +25,7 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar');
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
@@ -23,13 +33,35 @@ export default class Calculator extends Component {
     }
 
     addDigit(n) {
-        console.log(n);
+        // Caso já exista mais de um ponto na display
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        // Impede de o valor 0 fique á esquerda do digito adicionado
+        const clearDisplay = this.state.displayValue === '0' 
+            || this.state.clearDisplay
+        // Verifica se é necessário limpar a tela
+        const valorAtual = clearDisplay ? '' : this.state.displayValue 
+        const displayValue = valorAtual + n
+
+        // Adiciona valores
+        this.setState({ displayValue, clearDisplay: false })
+
+        // Caso não tenha ponto
+        if (n !== '.') {
+            const i = this.state.atual // Indice de alteração
+            const novoValor = parseFloat(displayValue) // Transforma em ponto flutuante
+            const values = [...this.state.values] // Clona os valores
+            values[i] = novoValor // Altera o o valor a partir do indice
+            this.setState({ values })
+        }
     }
 
     render() {
         return (
             <div className='calculadora'>
-                <Display value={100}/>
+                <Display value={this.state.displayValue}/>
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>

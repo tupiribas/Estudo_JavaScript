a521c1b812663ad1755ad1dd12ef631b49a4b4da
Tupi Guedes <tupiguedes@outlook.com>
2022-07-22 23:41:25 -0300

    Cadastro de usuário

--- react/crud/frontend/src/components/user/UserCrud.jsx
+++ react/crud/frontend/src/components/user/UserCrud.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axius from 'axios'
+
 import Main from "../template/Main";
 
 const headerProps = {
@@ -7,7 +9,42 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+
+    state = { ...initialState }
+
+    // Limpar apenas o usuário
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        // Esolha da requisição http que será feita
+        const methodHTTP = user.id ? 'put' : 'post'
+        // Incrementa a url de base com o id do usuário
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+
+        axius[methodHTTP](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+                this.setState({ user: initialState.user, list }) // Atualizar os dados
+            })
+    }
+
+    getUpdatedList(user) {
+        // varificar se o usuário já foi acrecentado á lista
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
